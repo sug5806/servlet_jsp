d6381d1d6e4db81add57f542a7a8b42c56354be4
홍동수 <sug5806@naver.com>
2021-06-06 17:57:08 +0900

    RequestMapping 리팩토링 static 제거

--- src/main/java/next/controller/RequestMapping.java
+++ src/main/java/next/controller/RequestMapping.java
@@ -1,12 +1,17 @@
 package next.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestMapping {
-    public static Map<String, Controller> controllerMap = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    static {
+    public Map<String, Controller> controllerMap = new HashMap<>();
+
+    void initMapping() {
         controllerMap.put("/users", new ListUserController());
         controllerMap.put("/users/create", new CreateUserController());
         controllerMap.put("/users/login", new LoginController());
@@ -14,5 +19,17 @@ public class RequestMapping {
         controllerMap.put("/users/form", new ForwardController("/user/form.jsp"));
         controllerMap.put("/users/loginForm", new ForwardController("/user/login.jsp"));
         controllerMap.put("/", new ForwardController("index.jsp"));
+
+        logger.info("RequestMapping Init!");
     }
+
+    public Controller findController(String url) {
+        return controllerMap.get(url);
+    }
+
+    void put(String url, Controller controller) {
+        controllerMap.put(url, controller);
+    }
+
+
 }
